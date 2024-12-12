6fbeea0f2131e4b36e691c4ac5e40a40b5fa8f61
Dmitry Popov <dmitrypopov31@gmail.com>
2024-12-12 15:43:36 +0300

    fixed tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,17 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react';
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
-import {
-  renderWithTranslation,
-} from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { renderCopmonents } from 'shared/lib/tests/renderComponents/renderComponents';
 
 describe('Sidebar', () => {
   test('with only first param', () => {
-    renderWithTranslation(<Sidebar />);
+    renderCopmonents(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
 
   test('test toggle', () => {
-    renderWithTranslation(<Sidebar />);
+    renderCopmonents(<Sidebar />);
     const toggleBtn = screen.getByTestId('sidebar-toggle');
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     fireEvent.click(toggleBtn);
