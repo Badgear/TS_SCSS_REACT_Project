a16290f549cb6a3e484bc0289a1c02a3aac01353
Dmitry Popov <dmitrypopov31@gmail.com>
2024-12-12 14:50:26 +0300

    added icons

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,7 +3,13 @@ import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { Button, ButtonSize, ThemeButton } from 'shared/ui/Button/Button';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+
+import MainIcon from 'shared/assets/icons/main-icon.svg';
+import AboutIcon from 'shared/assets/icons/about-icon.svg';
+
 import styles from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -29,6 +35,28 @@ export const Sidebar = ({ className }: SidebarProps) => {
         [className]
       )}
     >
+      <div className={styles.links}>
+        <div>
+          <AppLink
+            theme={AppLinkTheme.SECONDARY}
+            to={RoutePath.main}
+            className={styles.linkWrapper}
+          >
+            <MainIcon className={styles.icon} />
+            <span className={styles.link}>{t('Главная')}</span>
+          </AppLink>
+        </div>
+        <div>
+          <AppLink
+            theme={AppLinkTheme.SECONDARY}
+            to={RoutePath.about}
+            className={styles.linkWrapper}
+          >
+            <AboutIcon className={styles.icon} />
+            <span className={styles.link}>{t('О сайте')}</span>
+          </AppLink>
+        </div>
+      </div>
       <Button
         data-testid='sidebar-toggle'
         onClick={onToggle}
@@ -39,6 +67,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
       >
         {collapsed ? '>' : '<'}
       </Button>
+
       <div className={styles.switchers}>
         <ThemeSwitcher />
         <LangSwitcher short={collapsed} className={styles.lang} />
